bff3336822099bfcff6802c70a1441068b934ae5
Stephen Moon <smoon@spiria.com>
2020-06-05 11:04:40 -0400

    Added crop view controller for image crop & zooming

---
+++ source/Avecare.code/Common/UI/ImageCropViewController.swift
@@ -0,0 +1,60 @@
+import UIKit
+import CropPickerView
+import CocoaLumberjack
+
+class ImageCropViewController: UIViewController {
+    @IBOutlet weak var cropPickerView: CropPickerView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var finishButton: UIButton!
+
+    var image: UIImage? {
+        didSet {
+            if cropPickerView != nil {
+                cropPickerView.image = image
+            }
+        }
+    }
+    private var resultImage: UIImage?
+
+    var onCancel: (() -> Void)?
+    var onFinish: ((_ image: UIImage?) -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let image = image {
+            cropPickerView.image = image
+        }
+
+        cancelButton.setTitle(NSLocalizedString("barbutton_title_cancel", comment: ""), for: .normal)
+        finishButton.setTitle(NSLocalizedString("barbutton_title_done", comment: ""), for: .normal)
+    }
+
+    @IBAction func cancelButtonTouched(_ sender: UIButton) {
+        if let onCancel = onCancel {
+            onCancel()
+        }
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func finishButtonTouched(_ sender: UIButton) {
+        cropPickerView.crop { (error, image) in
+            if let error = error as NSError? {
+                DDLogError(error.localizedDescription)
+                self.onFinish?(nil)
+            } else {
+                self.onFinish?(image)
+            }
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+}
+
+extension UIViewController {
+    func presentCropPicker(_ cropPicker: ImageCropViewController, animated: Bool = true, cancel: (() -> Void)?, finish: ((UIImage?) -> Void)?, completion: (() -> Void)? = nil) {
+        cropPicker.onCancel = cancel
+        cropPicker.onFinish = finish
+
+        self.present(cropPicker, animated: animated, completion: completion)
+    }
+}
