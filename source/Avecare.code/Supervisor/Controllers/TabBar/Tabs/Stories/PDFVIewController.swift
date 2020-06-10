8c17640cf2dc02a2847fb0ceede1d41cea4e7a8a
syed haider <shaider@spiria.com>
2020-06-10 15:31:04 -0400

    pdf view alignments

--- source/Avecare.code/Supervisor/Controllers/TabBar/Tabs/Stories/PDFVIewController.swift
+++ source/Avecare.code/Supervisor/Controllers/TabBar/Tabs/Stories/PDFVIewController.swift
@@ -19,24 +19,30 @@ class PDFVIewController: UIViewController {
         pdfView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(pdfView)
 
-        pdfView.leadingAnchor.constraint(equalTo: innerView.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        pdfView.trailingAnchor.constraint(equalTo: innerView.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        pdfView.topAnchor.constraint(equalTo: innerView.safeAreaLayoutGuide.topAnchor).isActive = true
-        pdfView.bottomAnchor.constraint(equalTo: innerView.safeAreaLayoutGuide.bottomAnchor).isActive = true
         pdfView.backgroundColor =  UIColor.white
+        pdfView.autoScales = true
 
         // Adding the thumnails
         let thumbnailView = PDFThumbnailView()
         thumbnailView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(thumbnailView)
 
-        thumbnailView.leadingAnchor.constraint(equalTo: innerView.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        thumbnailView.bottomAnchor.constraint(equalTo: innerView.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        thumbnailView.topAnchor.constraint(equalTo: innerView.safeAreaLayoutGuide.topAnchor).isActive = true
         thumbnailView.widthAnchor.constraint(equalToConstant: 70).isActive = true
         thumbnailView.backgroundColor = UIKit.UIColor(resource: R.color.background, compatibleWith: nil) ?? UIColor.white
         thumbnailView.thumbnailSize = CGSize(width: 70, height: 70)
-        thumbnailView.layoutMode = .vertical
+
+        pdfView.snp.makeConstraints { (make) in
+            make.top.equalTo(self.innerView.snp.top).offset(0)
+            make.bottom.equalTo(self.innerView.snp.bottom).offset(0)
+            make.leading.equalTo(self.innerView.snp.leading).offset(90)
+            make.trailing.equalTo(self.innerView.snp.trailing).offset(0)
+        }
+        thumbnailView.snp.makeConstraints { (make) in
+            make.top.equalTo(self.innerView.snp.top).offset(0)
+            make.bottom.equalTo(self.innerView.snp.bottom).offset(0)
+            make.leading.equalTo(self.innerView.snp.leading).offset(0)
+            make.trailing.equalTo(pdfView.snp.leading).offset(0)
+        }
         thumbnailView.pdfView = pdfView
         if let url = url, let document = PDFDocument(url: url) {
             pdfView.document = document
