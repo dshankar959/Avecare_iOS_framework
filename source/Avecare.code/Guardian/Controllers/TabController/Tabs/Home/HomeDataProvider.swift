4f013cefe15eed7c57434171a495b99c74214634
Stephen Moon <smoon@spiria.com>
2020-06-25 11:47:22 -0400

    Resolved AVC-123 - Add subtitle of date to daily-log row in home feed

--- source/Avecare.code/Guardian/Controllers/TabController/Tabs/Home/HomeDataProvider.swift
+++ source/Avecare.code/Guardian/Controllers/TabController/Tabs/Home/HomeDataProvider.swift
@@ -226,7 +226,7 @@ extension HomeTableViewDisclosureCellModel {
             let subjectForLog = subject,
             let documentStorage = storage {
             title = subjectForLog.firstName + NSLocalizedString("home_feed_title_dailylog", comment: "")
-            subtitle = nil
+            subtitle = feed.date.dateStringHumanFriendly
             subjectImageURL = subjectForLog.photoURL(using: documentStorage)
         } else {
             title = feed.header
